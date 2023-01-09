95629387407f2c859f58c1aec267a531c8bc3380
Ray Bellis <ray@isc.org>
2023-01-09 01:32:37 +0000

    massive refactor, with nascent support for multiple HDUs

---
+++ index.mjs
@@ -0,0 +1,9 @@
+/*
+ * FITS file handler, specification at:
+ * https://fits.gsfc.nasa.gov/standard40/fits_standard40aa-le.pdf
+ */
+
+import File from './lib/file.mjs';
+import Header from './lib/header.mjs';
+
+export { File, Header };
